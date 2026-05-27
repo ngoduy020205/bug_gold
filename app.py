aced55f8efacf0d92479eca15bc1be0d6e96afdc
ngoduy020205 <acechuon92@gmail.com>
2026-05-27 20:08:54 +0700

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ CORS(app)
 
 @app.route('/buy')
 def buy():
-    srv_id = request.args.get('srv_id')
+    srv_id = request.args.get('srv_id', '')
     role_id = request.args.get('role_id')
     name = request.args.get('name')
     productId = request.args.get('productId', '21')
@@ -20,12 +20,23 @@ def buy():
     cookie = "PHPSESSID=bskoh80nq21rlulqpakotjdaq6"
     
     # Phân luồng Server máy chủ
-    if game_id == '5': # Uchia Truyền Kỳ (Đã cập nhật IP & Port chuẩn, bỏ tham số app)
-        base_url = "http://103.149.252.111:92/api.php/pf/sygame/pay_web/" 
-    elif game_id == '4': # Học Viện Shinobi
+    if game_id == '5': # Uchia Truyền Kỳ (Có Port động)
+        try:
+            # Tách số ra từ chuỗi "symlf_X" (VD: "symlf_2" -> "2")
+            server_num = int(srv_id.replace("symlf_", ""))
+            port = 90 + server_num
+        except:
+            # Trừ hao nếu nhập linh tinh thì lấy Port 91 làm gốc
+            port = 91
+            
+        base_url = f"http://103.149.252.111:{port}/api.php/pf/sygame/pay_web/" 
+        
+    elif game_id == '4': # Học Viện Shinobi (Port cố định 100 theo bạn gửi trước đó)
         base_url = "http://180.93.98.5:100/api.php/pf/sygame/pay_web/"
+        
     elif game_id == '3': # Hokage Saga
         base_url = "http://160.30.113.78:1118/api.php/pf/sygame/pay_web/"
+        
     else: 
         base_url = "http://nhangiatruyenky.com:126/api.php/pf/sygame/pay_web/"
         
