9d1b9b78f8c89f041816a9de60180b25b16ee51c
ngoduy020205 <acechuon92@gmail.com>
2026-05-27 18:40:37 +0700

    Update app.py

--- app.py
+++ app.py
@@ -12,21 +12,21 @@ def buy():
     name = request.args.get('name')
     productId = request.args.get('productId', '21')
     
-    # Lấy thông số nhận diện Game từ Web (3: Hokage, 4: Học Viện)
-    game_id = request.args.get('game', '4') 
+    # Lấy thông số nhận diện Game từ Web (3: Hokage, 4: Học Viện, 5: Uchia)
+    game_id = request.args.get('game', '5') 
     money = "1"
     
-    # Dùng chung Session ID
+    # Dùng chung Session ID cố định
     cookie = "PHPSESSID=bskoh80nq21rlulqpakotjdaq6"
     
-    # Phân luồng Server
-    if game_id == '4': # Học Viện Shinobi
+    # Phân luồng Server máy chủ
+    if game_id == '5': # Uchia Truyền Kỳ (Đã cập nhật IP & Port chuẩn, bỏ tham số app)
+        base_url = "http://103.149.252.111:92/api.php/pf/sygame/pay_web/" 
+    elif game_id == '4': # Học Viện Shinobi
         base_url = "http://180.93.98.5:100/api.php/pf/sygame/pay_web/"
     elif game_id == '3': # Hokage Saga
         base_url = "http://160.30.113.78:1118/api.php/pf/sygame/pay_web/"
-    elif game_id == '2': # Shinobi Classic (Cũ)
-        base_url = "http://160.30.112.91:96/api.php/pf/sygame/pay_web/"
-    else: # Nhẫn Giả (Cũ)
+    else: 
         base_url = "http://nhangiatruyenky.com:126/api.php/pf/sygame/pay_web/"
         
     url = f"{base_url}?srv_id={srv_id}&role_id={role_id}&money={money}&name={name}&productId={productId}"
